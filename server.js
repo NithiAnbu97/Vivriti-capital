4d41e56e74dc423bdea985d2b86721352df17263
Nithiyanantham S A <nithianbu97@gmail.com>
2022-11-20 17:06:43 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+const UserRoute = require('./app/routes/User')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
+
+app.use('/user',UserRoute)
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Database Connected Successfully!!");    
+}).catch(err => {
+    console.log('Could not connect to the database', err);
+    process.exit();
+});
+
